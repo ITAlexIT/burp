c42655314a2ec6bf092b607cc72a4b68c1695221
ITAlexIT <60471197+ITAlexIT@users.noreply.github.com>
2021-08-28 12:02:45 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 sudo apt update 
-sudo apt install screen -y
 sudo apt install htop
 sudo fallocate -l 2G /swapfile2 && sudo chmod 600 /swapfile2 && sudo mkswap /swapfile2 && sudo swapon /swapfile2 && echo '/swapfile2 none swap sw 0 0' | sudo tee -a /etc/fstab
 sudo apt-get install  curl apt-transport-https ca-certificates software-properties-common -y
