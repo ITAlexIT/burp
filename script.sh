f3f01530238c728eea34587a2d3161d35157007f
ITAlexIT <60471197+ITAlexIT@users.noreply.github.com>
2021-08-29 16:12:09 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt update 
-sudo apt install htop
+sudo apt install htop screen -y
 sudo fallocate -l 2G /swapfile2 && sudo chmod 600 /swapfile2 && sudo mkswap /swapfile2 && sudo swapon /swapfile2 && echo '/swapfile2 none swap sw 0 0' | sudo tee -a /etc/fstab
 sudo apt-get install  curl apt-transport-https ca-certificates software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
